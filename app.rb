736c703dfbc8f94675865fc335d955195b08b8cc
WModig <william.modig@elev.ga.ntig.se>
2023-01-31 11:13:09 +0100

    Update & add new

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions
+
+get('/') do
+    slim(:start)
+end
+
+get('/logs') do
+    db = SQLite3::Database.new("db/traningslogg.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM logs")
+    p result
+    slim(:"logs/index",locals:{logs:result})
+end
+  
+get('/logs/new') do
+    slim(:"logs/new")
+end
+
+post('/logs/new') do
+    user_id = params[:user_id]
+    content = params[:content]
+    date = params[:date]
+    db = SQLite3::Database.new("db/traningslogg.db")
+    db.execute("INSERT INTO logs (user_id, content, date) VALUES (?,?,?)",user_id,content,date)
+    slim(:"logs/new")
+end
+
+
+get('/logs/:id/edit') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/traningslogg.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM logs WHERE id = ?",id).first
+    slim(:"logs/edit",locals:{result:result})
+end
+
+post('/logs/:id/update') do
+    content = params[:content]
+    date = params[:date]
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/traningslogg.db")
+    db.execute("UPDATE logs SET content = ?,date = ? WHERE id = ?",content,date,id)
+    redirect('/logs')
+  end
